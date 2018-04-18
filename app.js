a4a3cf69e8d7f3e1afff4e57f8ead45702653030
R1zzo23 <adamjrizzo@yahoo.com>
2018-04-18 11:41:47 -0400

    Added About Me and testimonials.

--- app.js
+++ app.js
@@ -1,16 +1,45 @@
 $(document).ready(function () {
   $('#webDevProjects').hide();
   $('#softwareDevProjects').hide();
+  $('#aboutMe').hide();
 });
+
+function aboutMeClick() {
+  var software = $('#softwareDevProjects');
+  var webDev = $('#webDevProjects');
+  var aboutMe = $('#aboutMe');
+  if(software.attr('show') == 'yes'){
+    software.fadeOut('fast', function(){
+      fadeProjects(aboutMe);
+    });
+    software.attr('show', 'no');
+  }
+  else if(webDev.attr('show') == 'yes'){
+    webDev.fadeOut('fast', function(){
+      fadeProjects(aboutMe);
+    });
+    webDev.attr('show', 'no');
+  }
+  else {
+    fadeProjects(aboutMe);
+  }
+}
 
 function webSkillClick() {
   var software = $('#softwareDevProjects');
   var webDev = $('#webDevProjects');
+  var aboutMe = $('#aboutMe');
   if(software.attr('show') == 'yes'){
     software.fadeOut('fast', function(){
       fadeProjects(webDev);
     });
     software.attr('show', 'no');
+  }
+  else if(aboutMe.attr('show') == 'yes'){
+    aboutMe.fadeOut('fast', function(){
+      fadeProjects(webDev);
+    });
+    aboutMe.attr('show', 'no');
   }
   else {
     fadeProjects(webDev);
@@ -20,11 +49,18 @@ function webSkillClick() {
 function softwareSkillClick() {
   var software = $('#softwareDevProjects');
   var webDev = $('#webDevProjects');
+  var aboutMe = $('#aboutMe');
   if(webDev.attr('show') == 'yes'){
     webDev.fadeOut('fast', function(){
       fadeProjects(software);
     });
     webDev.attr('show', 'no');
+  }
+  else if(aboutMe.attr('show') == 'yes'){
+    aboutMe.fadeOut('fast', function(){
+      fadeProjects(webDev);
+    });
+    aboutMe.attr('show', 'no');
   }
   else {
     fadeProjects(software);
